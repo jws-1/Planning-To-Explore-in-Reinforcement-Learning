28645b7b859cda0aa15f2f42e982ec0f631eb6c9
Jared Swift <j.w.swift@outlook.com>
2023-03-28 16:39:02 +0100

    feat: started learning meta actions.

--- src/fyp/stochastic_transitions/agents/meta_prl_agent.py
+++ src/fyp/stochastic_transitions/agents/meta_prl_agent.py
@@ -2,8 +2,9 @@ from copy import deepcopy
 from .rl_agent import RLAgent
 import numpy as np
 import random
-from ..models.mdp import MetaAction
-
+# from ..models.mdp import MetaAction
+from ..actions import MetaAction
+from collections import defaultdict
 class MetaPRLAgent(RLAgent):
 
     def __init__(self, env, model):
@@ -16,16 +17,15 @@ class MetaPRLAgent(RLAgent):
         self.model = deepcopy(self.initial_model)
         self.N_sa = {state : {action : 0 for action in range(self.env.nA)} for state in range(self.env.nS)}
         self.N_sas = {state : {action : {next_state : 0 for next_state in range(self.env.nS)} for action in range(self.env.nA)} for state in range(self.env.nS)}
-        self.meta_sas = {state : {action : {next_state: {meta_action : False for meta_action in MetaAction} for next_state in range(self.env.nS)} for action in range(self.env.nA)} for state in range(self.env.nS)}
+        self.meta_sas = {state : {action : {next_state: defaultdict(bool) for next_state in range(self.env.nS)} for action in range(self.env.nA)} for state in range(self.env.nS)}
 
     def learn(self, config):
         self.reset()
-
         rewards = np.zeros(config.episodes)
         states = np.zeros((config.episodes, self.env.nS))
         next_action = None
         self.observed_sas = {state : {action : {next_state: False for next_state in range(self.env.nS)} for action in range(self.env.nA)} for state in range(self.env.nS)}
-
+        self.meta_actions = []
         for i in range(config.episodes):
             # if i % (config.episodes // 10) == 0:
             print(f"Meta PRL-AGENT: episode {i}")
@@ -44,7 +44,7 @@ class MetaPRLAgent(RLAgent):
                     action = next_action
                     next_action = None
                 elif planning:
-                    plan = self.model.plan_VI(state, meta=True, observed_sas=self.observed_sas, meta_sas=self.meta_sas)
+                    plan = self.model.plan_VI(state, meta=True, observed_sas=self.observed_sas, meta_sas=self.meta_sas, meta_actions = self.meta_actions)
                     if isinstance(plan, tuple):
                         if len(plan) == 3:
                             action, target_action, target_state = plan
@@ -55,13 +55,16 @@ class MetaPRLAgent(RLAgent):
                     action = random.choice([a for a in range(self.env.nA) if self.Q[state][a] == max(self.Q[state].values())])
 
                 if isinstance(action, MetaAction):
-                    if action == MetaAction.INCREASE_REWARD:
-                        # print(state, action, target_action, target_state)
-                        self.model.update_reward(state, target_action, target_state, np.max(self.model.reward_function))
-                    elif action == MetaAction.INCREASE_TRANSITION_PROBABILITY:
-                        # print(state, action, target_action, target_state)
-                        self.model.update_transition_prob(state, target_action, target_state, 1.0)
+                    self.model.update_transition_prob(state, target_action, target_state, 1.0)
                     self.meta_sas[state][target_action][target_state][action] = True
+                    # if action == MetaAction.INCREASE_REWARD:
+                    #     # print(state, action, target_action, target_state)
+                    #     self.model.update_reward(state, target_action, target_state, np.max(self.model.reward_function))
+                    # elif action == MetaAction.INCREASE_TRANSITION_PROBABILITY:
+                    #     # print(state, action, target_action, target_state)
+                    #     self.model.update_transition_prob(state, target_action, target_state, 1.0)
+                    # self.meta_sas[state][target_action][target_state][action] = True
+                    pass
                 else:
                     next_state, reward, done, _ = self.env.step(action)
                     # print(state, action, next_state)
@@ -78,9 +81,17 @@ class MetaPRLAgent(RLAgent):
                         self.model.update_reward(state, action, next_state, reward)
                         self.observed_sas[state][action][next_state ] = True
 
+                        if state != next_state:
+                            action_sequence = self.model.action_sequence(state, next_state)
+                            if len(action_sequence) > 1:
+                                meta_action = MetaAction(action, action_sequence)
+                                if not meta_action in self.meta_actions:
+                                    self.meta_actions.append(meta_action)
+                                    self.meta_sas[state][action][next_state][meta_action] = False
+                            
                     if state != next_state:
                         states[i][state]+=1
-                
+
                     state = next_state
                     rewards[i] += reward    
 
--- src/fyp/stochastic_transitions/models/mdp.py
+++ src/fyp/stochastic_transitions/models/mdp.py
@@ -5,12 +5,13 @@ from enum import Enum
 import numba
 from typing import Optional
 from itertools import product
+import heapq
 
-class MetaAction(Enum):
-    INCREASE_TRANSITION_PROBABILITY = 0
-    DECREASE_TRANSITION_PROBABILITY = 1
-    INCREASE_REWARD = 2
-    DECREASE_REWARD = 3
+# class MetaAction(Enum):
+#     INCREASE_TRANSITION_PROBABILITY = 0
+#     DECREASE_TRANSITION_PROBABILITY = 1
+#     INCREASE_REWARD = 2
+#     DECREASE_REWARD = 3
 
 
 @numba.jit(nopython=True)
@@ -60,9 +61,38 @@ class MDP:
             self.V = np.zeros(len(self.states))
             self.pi = np.zeros(len(self.states))
 
-
     def get_transition_probs(self, state, action):
         return self.transition_function[state, action]
+
+    def action_sequence(self, start_state, goal_state):
+        # Create a priority queue for uniform cost search
+        pq = [(0, start_state, [])]
+        visited = set()
+
+        # Run uniform cost search
+        while len(pq) > 0:
+            (cost, state, actions) = heapq.heappop(pq)
+            if state in visited:
+                continue
+            visited.add(state)
+
+            # Check if we have reached the goal state
+            if state == goal_state:
+                return actions
+
+            # Add neighboring states to the priority queue
+            for action in self.actions:
+                for next_state, prob in zip(self.states, self.get_transition_probs(state, action)):
+                    if prob > 0.0:
+                        heapq.heappush(pq, (cost + 1, next_state, actions + [action]))
+        # If we haven't found a path to the goal state, return None
+        return None
+
+
+    def simulate_action_sequence(self, state, action_sequence):
+        for action in action_sequence:
+            state, _ = self.step(state, action)
+        return state
 
     def update_transition_prob(self, state, action, next_state, prob):
         if prob == 1.0:
@@ -94,45 +124,53 @@ class MDP:
         next_state = np.random.choice(self.states, p=transition_probs)
         return next_state, self.get_reward(state, action, next_state)
 
-    def plan_VI(self, start, observed_sas=None, meta=None, meta_sas=None):
+    def plan_VI(self, start, observed_sas=None, meta=None, meta_sas=None, meta_actions=None):
         if self.updated:
             self.V, self.pi = value_iteration(self.V, self.states, self.actions, self.transition_function, self.reward_function, self.discount_factor, max_iter=10)
             self.updated = False
         if not meta:
             return self.pi[start]
-        """
-        Learn the depth
-            If we normally expect to get to a state through n transitions from the current state
-            but it actually takes m transitions,
-            then we learn that meta action
-        """
-        candidate_changes_r = {(start, a, next_state, np.max(self.reward_function)) : -np.inf for (a, next_state) in product(self.actions, self.states)}
-        candidate_changes_t = {(start, a, next_state) : -np.inf for (a, next_state) in product(self.actions, self.states)}
+        
 
-        for (s, a, s_, r) in candidate_changes_r.keys():
-            if not observed_sas[s][a][s_] and not meta_sas[s][a][s_][MetaAction.INCREASE_REWARD]:
-                candidate_MDP = deepcopy(self)
-                candidate_MDP.update_reward(s, a, s_, r)
-                V_, pi_ = value_iteration(deepcopy(self.V), candidate_MDP.states, candidate_MDP.actions, candidate_MDP.transition_function, candidate_MDP.reward_function, candidate_MDP.discount_factor, max_iter=10)
-                candidate_changes_r[(s,a,s_,r)] = V_[s]
+        candidate_changes_t = {}
+        for meta_action in meta_actions:
+            action, next_state = meta_action.action, self.simulate_action_sequence(start, meta_action.action_sequence)
+            candidate_changes_t[(start, action, meta_action, next_state)] = -np.inf
+        
 
-        for (s, a, s_) in candidate_changes_t.keys():
-            if not observed_sas[s][a][s_] and not meta_sas[s][a][s_][MetaAction.INCREASE_TRANSITION_PROBABILITY] and s_ in self.reasonable_meta_transitions[s] and self.transition_function[s, a, s_] < 1.0:
+        # """
+        # Learn the depth
+        #     If we normally expect to get to a state through n transitions from the current state
+        #     but it actually takes m transitions,
+        #     then we learn that meta action
+        # """
+        # candidate_changes_r = {(start, a, next_state, np.max(self.reward_function)) : -np.inf for (a, next_state) in product(self.actions, self.states)}
+        # candidate_changes_t = {(start, a, next_state) : -np.inf for (a, next_state) in product(self.actions, self.states)}
+
+        # for (s, a, s_, r) in candidate_changes_r.keys():
+        #     if not observed_sas[s][a][s_] and not meta_sas[s][a][s_][MetaAction.INCREASE_REWARD]:
+        #         candidate_MDP = deepcopy(self)
+        #         candidate_MDP.update_reward(s, a, s_, r)
+        #         V_, pi_ = value_iteration(deepcopy(self.V), candidate_MDP.states, candidate_MDP.actions, candidate_MDP.transition_function, candidate_MDP.reward_function, candidate_MDP.discount_factor, max_iter=10)
+        #         candidate_changes_r[(s,a,s_,r)] = V_[s]
+
+        for (s, a, m_a, s_) in candidate_changes_t.keys():
+            if not observed_sas[s][a][s_] and not meta_sas[s][a][s_][m_a] and self.transition_function[s, a, s_] < 1.0:
                 candidate_MDP = deepcopy(self)
                 candidate_MDP.update_transition_prob(s, a, s_, 1.0)
                 V_, pi_ = value_iteration(deepcopy(self.V), candidate_MDP.states, candidate_MDP.actions, candidate_MDP.transition_function, candidate_MDP.reward_function, candidate_MDP.discount_factor, max_iter=10)
 
                 candidate_changes_t[(s,a,s_)] = V_[s]
         
-        best_max_r = max(candidate_changes_r.values())
-        best_change_r = random.choice([c_r for c_r in candidate_changes_r.keys() if candidate_changes_r[c_r] == best_max_r])
-        best_max_t = max(candidate_changes_t.values())
-        best_change_t = random.choice([c_t for c_t in candidate_changes_t.keys() if candidate_changes_t[c_t] == best_max_t])
-        if self.V[start] > candidate_changes_r[best_change_r] and self.V[start] > candidate_changes_t[best_change_t]:
-            return self.pi[start]
-        elif candidate_changes_r[best_change_r] > candidate_changes_t[best_change_t] and candidate_changes_r[best_change_r] > self.V[start]:
-            _, target_action, target_state, _ = best_change_r
-            return MetaAction.INCREASE_REWARD, target_action, target_state
-        else:
-            _, target_action, target_state = best_change_t
-            return MetaAction.INCREASE_TRANSITION_PROBABILITY, target_action, target_state
+        # best_max_r = max(candidate_changes_r.values())
+        # best_change_r = random.choice([c_r for c_r in candidate_changes_r.keys() if candidate_changes_r[c_r] == best_max_r])
+        # best_max_t = max(candidate_changes_t.values())
+        # best_change_t = random.choice([c_t for c_t in candidate_changes_t.keys() if candidate_changes_t[c_t] == best_max_t])
+        # if self.V[start] > candidate_changes_r[best_change_r] and self.V[start] > candidate_changes_t[best_change_t]:
+        #     return self.pi[start]
+        # elif candidate_changes_r[best_change_r] > candidate_changes_t[best_change_t] and candidate_changes_r[best_change_r] > self.V[start]:
+        #     _, target_action, target_state, _ = best_change_r
+        #     return MetaAction.INCREASE_REWARD, target_action, target_state
+        # else:
+        #     _, target_action, target_state = best_change_t
+        #     return MetaAction.INCREASE_TRANSITION_PROBABILITY, target_action, target_state
